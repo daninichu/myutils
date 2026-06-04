db6c30468446f42c2de5581fffdc525ad17599da
daninichu <danielnguyen6@icloud.com>
2026-06-04 22:42:58 +0200

    ArrayGrid size

--- src/main/java/com/daninichu/util/ArrayGrid.java
+++ src/main/java/com/daninichu/util/ArrayGrid.java
@@ -16,6 +16,7 @@ import java.util.function.UnaryOperator;
 @SuppressWarnings("unchecked")
 public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 	private final Object[][] data;
+	private int size;
 
 	/**
 	 * Sets the dimensions of the grid.
@@ -26,11 +27,12 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
     public ArrayGrid(int width, int height){
 		if(width < 1 || height < 1)
             throw new IllegalArgumentException("width and height must be 1 or greater");
-        this.data = new Object[height][width];
+        data = new Object[height][width];
     }
 
     public ArrayGrid(ArrayGrid<? extends E> grid){
-		this.data = grid.toArray();
+		data = grid.toArray();
+		size = grid.size();
     }
 
 	public int width(){
@@ -69,6 +71,11 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 		}
 	}
 
+	@Override
+	public int size(){
+		return size;
+	}
+
 	/**
 	 * @return {@inheritDoc}
 	 * @throws NullPointerException {@inheritDoc}
@@ -98,6 +105,8 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 		checkInBounds(x, y);
 		E oldValue = (E) data[y][x];
 		data[y][x] = Objects.requireNonNull(e);
+		if(oldValue == null)
+            size++;
 		return oldValue;
     }
 
@@ -126,9 +135,11 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
     @Override
     public E removePoint(int x, int y){
 		checkInBounds(x, y);
-        E e = (E) data[y][x];
+        E oldValue = (E) data[y][x];
 		data[y][x] = null;
-		return e;
+		if(oldValue != null)
+			size--;
+		return oldValue;
     }
 
 	/**
@@ -143,15 +154,16 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 	@Override
 	public boolean removeValue(E e){
         if(e != null){
-			int height = height();
-			int width = width();
 			Object[][] data = this.data;
+			int height = data.length;
+			int width = data[0].length;
 			for(int y = 0; y < height; y++){
-				Object[] inner = data[y];
+				Object[] row = data[y];
 				for(int x = 0; x < width; x++){
-					Object o = inner[x];
+					Object o = row[x];
                     if(o != null && e.equals(o)){
-                        inner[x] = null;
+                        row[x] = null;
+						size--;
                         return true;
                     }
                 }
@@ -194,13 +206,13 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 	@Override
 	public Point pointOf(E e){
 		if(e != null){
-			int height = height();
-			int width = width();
 			Object[][] data = this.data;
+			int height = data.length;
+			int width = data[0].length;
 			for(int y = 0; y < height; y++){
-				Object[] inner = data[y];
+				Object[] row = data[y];
 				for(int x = 0; x < width; x++){
-					Object o = inner[x];
+					Object o = row[x];
                     if(o != null && e.equals(o)){
                         return new Point(x, y);
                     }
@@ -213,37 +225,69 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 	public void fill(E e){
 		Objects.requireNonNull(e);
 		Object[][] data = this.data;
-		for(int y = 0, height = height(); y < height; y++)
-            Arrays.fill(data[y], e);
+		int height = data.length;
+		int width = data[0].length;
+		for(int y = 0; y < height; y++){
+			Object[] row = data[y];
+			for(int x = 0; x < width; x++){
+				if(row[x] == null){
+					row[x] = e;
+					size++;
+				}
+			}
+        }
 	}
 
 	public void fillRow(int y, E e){
 		Objects.requireNonNull(e);
-		Arrays.fill(data[y], e);
+		Object[] row = this.data[y];
+		for(int x = 0, width = row.length; x < width; x++){
+			if(row[x] == null){
+				row[x] = e;
+                size++;
+            }
+		}
 	}
 
 	public void fillCol(int x, E e){
 		Objects.requireNonNull(e);
 		Object[][] data = this.data;
-		for(int y = 0, height = height(); y < height; y++)
-            data[y][x] = e;
+		for(int y = 0, height = data.length; y < height; y++){
+			Object[] row = data[y];
+			if(row[x] == null){
+				row[x] = e;
+                size++;
+            }
+		}
 	}
 
 	@Override
 	public void clear(){
 		Object[][] data = this.data;
-        for(int y = 0, height = height(); y < height; y++)
+		for(int y = 0, height = data.length; y < height; y++)
             Arrays.fill(data[y], null);
+		size = 0;
 	}
 
 	public void clearRow(int y){
-		Arrays.fill(data[y], null);
+		Object[] row = this.data[y];
+		for(int x = 0, width = row.length; x < width; x++){
+			if(row[x] != null){
+				row[x] = null;
+                size--;
+            }
+		}
 	}
 
 	public void clearCol(int x){
 		Object[][] data = this.data;
-		for(int y = 0, height = height(); y < height; y++)
-            data[y][x] = null;
+		for(int y = 0, height = data.length; y < height; y++){
+			Object[] row = data[y];
+			if(row[x] != null){
+				row[x] = null;
+                size--;
+            }
+		}
 	}
 
 	@Override
@@ -290,9 +334,9 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 			int height = height();
 			int width = width();
 			while(y < height){
-				Object[] inner = data[y];
+				Object[] row = data[y];
 				while(x < width){
-					if(inner[x] != null)
+					if(row[x] != null)
                         return true;
 					x++;
 				}
@@ -307,6 +351,7 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 			if(lastY == -1)
 				throw new IllegalStateException();
 			data[lastY][lastX] = null;
+			size--;
 			lastY = -1;
 		}
 	}
@@ -314,7 +359,18 @@ public class ArrayGrid<E> extends AbstractGrid<E> implements Grid<E>{
 	@Override
 	public void compute(int x, int y, UnaryOperator<E> operator){
 		checkInBounds(x, y);
-		data[y][x] = operator.apply((E) data[y][x]);
+		Object[] row = this.data[y];
+		E e = (E) row[x];
+		E result = operator.apply(e);
+		if(e == null){
+			if(result != null){
+				row[x] = result;
+				size++;
+			}
+		} else if(result == null){
+            row[x] = null;
+            size--;
+        }
 	}
 
 	public E[][] toArray(){
--- src/test/java/com/daninichu/benchmark/grid/ArrayGridBenchmark.java
+++ src/test/java/com/daninichu/benchmark/grid/ArrayGridBenchmark.java
@@ -2,8 +2,6 @@ package com.daninichu.benchmark.grid;
 
 import com.daninichu.benchmark.Main;
 import com.daninichu.util.ArrayGrid;
-import com.daninichu.util.FlatArrayGrid;
-import com.daninichu.util.Grid;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -15,76 +13,42 @@ import java.util.concurrent.TimeUnit;
 })
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Warmup(		iterations = 2, 	time = 1000, 	timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(	iterations = 5, 	time = 1000, 	timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(	iterations = 4, 	time = 1000, 	timeUnit = TimeUnit.MILLISECONDS)
 @Fork(1)
 @State(Scope.Thread)
 public class ArrayGridBenchmark{
-
 	public static void main(String[] args) throws Exception{
 		Main.benchmark(ArrayGridBenchmark.class);
 	}
 
-	@Param({
-			"1000000",
-	}) int n;
-	@Param({
-//			"0",
-//			"1",
-			"50",
-//			"99",
-//			"100",
-	})
-	int widthPercent;
-
-	int width, height;
+	int width = 1000000;
+	int height = 40;
 
 	ArrayGrid<Object> arrayGrid;
-//	FlatArrayGrid<Object> flatArrayGrid;
 
 	@Setup(Level.Iteration)
 	public void setup2(){
 		arrayGrid.clear();
-//		flatArrayGrid.clear();
 	}
+
 	@Setup(Level.Trial)
 	public void setup(){
-		if(widthPercent == 0){
-			width = 1;
-			height = n;
-		} else if(widthPercent == 100){
-			width = n;
-			height = 1;
-		} else{
-			width = n * widthPercent / 100;
-			height = n / width;
-		}
-
 		arrayGrid = new ArrayGrid<>(width, height);
-//		flatArrayGrid = new FlatArrayGrid<>(width, height);
 	}
 
 	@Benchmark
 	public void arrayGrid(Blackhole bh){
-		int i = 0;
-		for(int y = 0; y < height; y++){
-			for(int x = 0; x < width; x++){
-//				Object e = 0;
-//				arrayGrid.set(x, y, i++);
-				bh.consume(arrayGrid.get(new Grid.Point(x, y)));
-			}
-		}
+		arrayGrid.fill(111);
 		bh.consume(arrayGrid);
+		if(arrayGrid.size() != width * height){
+			throw new AssertionError();
+		}
 	}
-//	@Benchmark
-//	public void flatArrayGrid(Blackhole bh){
-//		int i = 0;
-//		for(int y = 0; y < height; y++){
-//			for(int x = 0; x < width; x++){
-////				Object e = 0;
-////				flatArrayGrid.set(x, y, i++);
-//				bh.consume(flatArrayGrid.get(x, y));
-//			}
-//		}
-//		bh.consume(flatArrayGrid);
-//	}
+	@Benchmark
+	public void arrayGrid2(Blackhole bh){
+		bh.consume(arrayGrid);
+		if(arrayGrid.size() != width * height){
+//			throw new AssertionError();
+		}
+	}
 }
